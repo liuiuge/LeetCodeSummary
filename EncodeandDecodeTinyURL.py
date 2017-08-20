0bc0881f506cd137f8651b99fd9b921bad805e94
liuiuge <1220595883@qq.com>
2017-08-19 18:41:58 -0700

    EncodeandDecodeTinyURL

---
+++ EncodeandDecodeTinyURL.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+class Codec:
+    def __init__(self):
+        self.urls = []
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        self.urls.append(longUrl)
+        return 'http://tinyurl.com/' + str(len(self.urls) - 1)
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.urls[int(shortUrl.split('/')[-1])]
